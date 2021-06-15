62f20c6dd41caef8f38defe68a338ec17863fa08
patell997 <73403420+patell997@users.noreply.github.com>
2021-06-15 12:55:48 +0530

    Add files via upload

---
+++ Exercise_1.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[68]:
+
+
+ice_cream_rating = 9
+sleeping_rating = 5
+
+
+# In[96]:
+
+
+first_name = input('What is your first name:')
+print('My First Name is:', first_name)
+
+
+# In[78]:
+
+
+last_name = input('What is your last name:')
+print('My Last Name is:',last_name)
+
+
+# In[97]:
+
+
+
+my_name = first_name,"",last_name
+print('my_name:', first_name,"",last_name)
+
+
+# In[16]:
+
+
+import math
+
+
+# In[69]:
+
+
+happiness_rating = (ice_cream_rating + sleeping_rating)/ 2
+print('happiness_rating is' , happiness_rating)
+
+
+# In[50]:
+
+
+type(ice_cream_rating), type(first_name), type(happiness_rating)
+
+
+# In[36]:
+
+
+type()
+
+
+# (int, str, float)
+# 
+
+# In[100]:
+
+
+
+print('My name is', first_name, 'and I give eating ice cream a score of', ice_cream_rating, 'out of 10!')
+print('I am', first_name,"",last_name,'and my sleeping enjoyment rating is', sleeping_rating ,'/ 10!')
+print('Based on the factors above, my happiness rating is', happiness_rating ,'out of 10, or', happiness_rating*10,'%!')
+
+
+# In[ ]:
+
+
+
+
